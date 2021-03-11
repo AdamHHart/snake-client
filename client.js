61a82c583880ce83187e78b643a5000508149691
Adam Hart <adamhayhart@gmail.com>
2021-03-10 16:34:32 -0800

    server recieves callbakc NAME: ___

--- client.js
+++ client.js
@@ -1,5 +1,6 @@
 // const { mod } = require("prelude-ls");
 const net = require('net');
+// const { stdin } = require('node:process');
 
 // const client = require('client');
 /**
@@ -12,6 +13,36 @@ const connect = function() {
   });
   // interpret incoming data as text
   conn.setEncoding('utf8'); 
+
+
+  // conn.on("connect", () => {
+  // console.log("Successfully connected to the game server. connectHandlerCallback");
+  // });
+
+  var readline = require('readline');
+
+  var rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+
+  });
+
+  conn.on('connect', () => {
+    console.log("Successfully connected to the game server. connectHandlerCallback");
+    rl.question(">>What's your name?  ", function(answer) {
+      console.log("Name: " + answer);
+      conn.write(answer);
+      rl.close();
+   });
+  });
+
+  // conn.on('connect', () => {
+  //   conn.write('Hello from client!');
+  //   conn.setEncoding('utf8'); // interpret data as text
+  //   conn.on('data', (data) => {
+  //     console.log('Message from client: ', data)
+  //   });
+  // });
 
   conn.on('data', (data) => {
     console.log(data.toString());
